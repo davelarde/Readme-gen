29a9ec3c4e540a184130cb432ab2c04589461931
danivelarde <danielavelarde4@gmail.com>
2022-04-20 21:12:46 -0400

    initials

---
+++ index.js
@@ -0,0 +1,57 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+inquirer.prompt([
+    {
+        type:'input',
+        name:'projectTitle',
+        message:'What is the Project Title?',
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'Write a brief description of your project',
+    },
+    {
+        type:'input',
+        name:'Installation',
+        message:'Describe the process installation',
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    v{
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    
+
+])
